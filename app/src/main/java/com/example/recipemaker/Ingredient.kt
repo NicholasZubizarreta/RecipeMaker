975bb29be302b06b2154f5cd7fa790a75914dcb5
NicholasZubizarreta <nickzubizarreta@me.com>
2021-03-25 18:06:01 -0400

    Initial commit

---
+++ app/src/main/java/com/example/recipemaker/Ingredient.kt
@@ -0,0 +1,6 @@
+package com.example.recipemaker
+
+//class for ingredients
+class Ingredient(var wholeNum: Int, var topHalf: Int, var bottomHalf: Int, var unit: String, val ingredient: String){
+
+}
